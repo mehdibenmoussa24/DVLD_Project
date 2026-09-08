dd94457b9d3730d8633c8537e8dbf627611db8ff
Mehdi Benmoussa <mben76337@gmail.com>
2026-09-08 14:38:26 +0100

    Add static CRUD methods to People class in DVLD.DataAccess.  With logging All methods use parameterized SQL and log exceptions.

--- DVLD.DataAccess/People.cs
+++ DVLD.DataAccess/People.cs
@@ -239,7 +239,234 @@ namespace DVLD.DataAccess
             return PersonID;
         }
 
-        
+        public static bool UpdatePerson(int PersonID, string FirstName, string SecondName,
+           string ThirdName, string LastName, string NationalNo, DateTime DateOfBirth,
+           short Gender, string Address, string Phone, string Email,
+            int NationalityCountryID, string ImagePath)
+        {
+
+            int rowsAffected = 0;
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+
+                string query = @"Update  People  
+                            set FirstName = @FirstName,
+                                SecondName = @SecondName,
+                                ThirdName = @ThirdName,
+                                LastName = @LastName, 
+                                NationalNo = @NationalNo,
+                                DateOfBirth = @DateOfBirth,
+                                Gender=@Gender,
+                                Address = @Address,  
+                                Phone = @Phone,
+                                Email = @Email, 
+                                NationalityCountryID = @NationalityCountryID,
+                                ImagePath =@ImagePath
+                                where PersonID = @PersonID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.Add("@PersonID", SqlDbType.Int).Value = PersonID;
+                    command.Parameters.Add("@FirstName", SqlDbType.NVarChar, 20).Value = FirstName;
+                    command.Parameters.Add("@SecondName", SqlDbType.NVarChar, 20).Value = SecondName;
+
+                    if (ThirdName != "" && ThirdName != null)
+                        command.Parameters.Add("@ThirdName", SqlDbType.NVarChar, 20).Value = ThirdName;
+                    else
+                        command.Parameters.Add("@ThirdName", SqlDbType.NVarChar, 20).Value = System.DBNull.Value;
+
+
+                    command.Parameters.Add("@LastName", SqlDbType.NVarChar, 20).Value = LastName;
+                    command.Parameters.Add("@NationalNo", SqlDbType.NVarChar, 20).Value = NationalNo;
+                    command.Parameters.Add("@DateOfBirth", SqlDbType.DateTime).Value = DateOfBirth;
+                    command.Parameters.Add("@Gender", SqlDbType.TinyInt).Value = Gender;
+                    command.Parameters.Add("@Address", SqlDbType.NVarChar, 500).Value = Address;
+                    command.Parameters.Add("@Phone", SqlDbType.NVarChar, 20).Value = Phone;
+
+                    if (Email != "" && Email != null)
+                        command.Parameters.Add("@Email", SqlDbType.NVarChar, 50).Value = Email;
+                    else
+                        command.Parameters.Add("@Email", SqlDbType.NVarChar, 50).Value = Email;
+
+                    command.Parameters.Add("@NationalityCountryID", SqlDbType.Int).Value = NationalityCountryID;
+
+                    if (ImagePath != "" && ImagePath != null)
+                        command.Parameters.Add("@ImagePath", SqlDbType.NVarChar, 250).Value = ImagePath;
+                    else
+                        command.Parameters.Add("@ImagePath", SqlDbType.NVarChar, 250).Value = ImagePath;
+
+
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogError("Error: " + ex.Message);
+                        return false;
+                    }
+                }
+             
+            }
+            return (rowsAffected > 0);
+        }
+
+        public static DataTable GetAllPeople()
+        {
+
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+
+                string query =
+                  @"SELECT People.PersonID, People.NationalNo,
+              People.FirstName, People.SecondName, People.ThirdName, People.LastName,
+			  People.DateOfBirth, People.Gender,  
+				  CASE
+                  WHEN People.Gender = 0 THEN 'Male'
+
+                  ELSE 'Female'
+
+                  END as GenderCaption ,
+			  People.Address, People.Phone, People.Email, 
+              People.NationalityCountryID, Countries.CountryName, People.ImagePath
+              FROM            People INNER JOIN
+                         Countries ON People.NationalityCountryID = Countries.CountryID
+                ORDER BY People.FirstName";
+
+
+
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        if (reader.HasRows)
+
+                        {
+                            dt.Load(reader);
+                        }
+
+                        reader.Close();
+
+
+                    }
+
+                    catch (Exception ex)
+                    {
+                        Logger.LogError("Error: " + ex.Message);
+                    }
+                }
+                 
+            }
+                    return dt;
+
+        }
+
+        public static bool DeletePerson(int PersonID)
+        {
+
+            int rowsAffected = 0;
+
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+
+                string query = @"Delete People where PersonID = @PersonID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.Add("@PersonID", SqlDbType.Int).Value = PersonID;
+
+                    try
+                    {
+                        connection.Open();
+
+                        rowsAffected = command.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogError("Error: " + ex.Message);
+                    }
+                 
+                }
+            }
+            return (rowsAffected > 0);
+
+        }
+
+        public static bool IsPersonExist(int PersonID)
+        {
+            bool isFound = false;
+
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+
+                string query = "SELECT Found=1 FROM People WHERE PersonID = @PersonID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.Add("@PersonID", SqlDbType.Int).Value = PersonID;
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        isFound = reader.HasRows;
+
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogError("Error: " + ex.Message);
+                        return false;
+                    }
+                 
+                }
+            }
+            return isFound;
+        }
+
+        public static bool IsPersonExist(string NationalNo)
+        {
+            bool isFound = false;
+
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+
+                string query = "SELECT Found=1 FROM People WHERE NationalNo = @NationalNo";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.Add("@NationalNo", SqlDbType.NVarChar, 20).Value = NationalNo;
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        isFound = reader.HasRows;
+
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogError("Error: " + ex.Message);
+                        isFound = false;
+                    }
+                }
+            }
+
+            return isFound;
+        }
 
     }
 }
