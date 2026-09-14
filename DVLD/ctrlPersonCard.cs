93c6fb91e35da7f9570ef9672d02e70253c8b335
Mehdi Benmoussa <mben76337@gmail.com>
2026-09-14 20:43:29 +0100

    add person details card with data loading and image support

--- DVLD/ctrlPersonCard.cs
+++ DVLD/ctrlPersonCard.cs
@@ -1,30 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using DVLD.Business;
+using DVLD.Properties;
 
 namespace DVLD
 {
     public partial class ctrlPersonCard : UserControl
     {
+        private Person _Person;
+     
         public ctrlPersonCard()
         {
             InitializeComponent();
+
+            
         }
 
-        private void ctrlPersonCard_Load(object sender, EventArgs e)
+        private void _LoadPersonImage()
         {
+            pbPersonalPicture.Image = _Person.Gender == 0 ? Resources.Man : Resources.Female;
+
+                string ImagePath = _Person.ImagePath;
+            if (ImagePath != "")
+            {
+                if (File.Exists(ImagePath))
+                    pbPersonalPicture.ImageLocation = ImagePath;
+                else
+                    MessageBox.Show("Could not find this image: = " + ImagePath, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
         }
 
-        private void panel13_Paint(object sender, PaintEventArgs e)
+        private void _FillPersonInfo()
         {
-
+            lblPersonID.Text = _Person.PersonID.ToString();
+            lblName.Text = _Person.FullName.ToString();
+            lblNationalNo.Text = _Person.NationalNo.ToString();
+            lblGender.Text = _Person.Gender == 0 ? "Male" : "Female";
+            lblEmail.Text = _Person.Email.ToString();
+            lblAddress.Text = _Person.Address.ToString();
+            lblDateOfBirth.Text = _Person.DateOfBirth.ToString();
+            lblPhone.Text = _Person.Phone.ToString();
+            lblCountry.Text = "Soon";
+            _LoadPersonImage();
         }
+
+        public void ResetPersonInfo()
+        {
+            lblPersonID.Text = "?????";
+            lblName.Text = "?????";
+            lblNationalNo.Text = "?????";
+            lblGender.Text = "?????";
+            lblEmail.Text = "?????";
+            lblAddress.Text = "?????";
+            lblDateOfBirth.Text = "?????";
+            lblPhone.Text = "?????";
+            lblCountry.Text = "?????";
+            pbPersonalPicture.Image = Resources.Man;
+        }
+
+        public void LoadPersonData(int PersonID)
+        {
+            _Person = Person.Find(PersonID);
+
+            if (_Person == null)
+            {
+                ResetPersonInfo();
+                MessageBox.Show("No Person with National No. = " + PersonID.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+           
+                _FillPersonInfo();
+ 
+        }
+
+        public void LoadPersonData(string NationalNo)
+        {
+            _Person = Person.Find(NationalNo);
+
+            if (_Person == null)
+            {
+                ResetPersonInfo();
+                MessageBox.Show("No Person with National No. = " + NationalNo.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+                _FillPersonInfo();
+        }
+       
+
+        
     }
 }
