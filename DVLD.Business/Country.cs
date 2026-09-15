e70454c5b409f73d51d1e6380f5ecd23831988a5
Mehdi Benmoussa <mben76337@gmail.com>
2026-09-15 11:04:34 +0100

    Add Country entity and data access for country info

---
+++ DVLD.Business/Country.cs
@@ -0,0 +1,56 @@
+﻿using DVLD.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Business
+{
+    public class Country
+    {
+        public int CountryID { get; set; }
+
+        public string CountryName { get; set; }
+
+        public Country()
+        {
+            CountryID = -1;
+            CountryName = string.Empty;
+        }
+
+
+        public Country(int countryID, string countryName) 
+        {
+            CountryID = countryID;
+            CountryName = countryName;
+        }
+
+        public static Country Find(int CountryID)
+        {
+            string CountryName = string.Empty;
+            
+            if(CountryData.GetCountryInfoByID(CountryID,ref CountryName))
+                return new Country(CountryID, CountryName);
+
+            return null;
+        }
+
+        public static Country Find(string CountryName)
+        {
+            int CountryID = -1;
+
+            if (CountryData.GetCountryInfoByName(CountryName, ref CountryID))
+                return new Country(CountryID, CountryName);
+
+            return null;
+        }
+
+        public static DataTable GetAllCountries()
+        {
+            return CountryData.GetAllCountries();
+
+        }
+    }
+}
---
+++ DVLD.DataAccess/CountryData.cs
@@ -0,0 +1,112 @@
+﻿using Common;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD.DataAccess
+{
+    public class CountryData
+    {
+      
+        public static bool GetCountryInfoByID(int CountryID,ref string CountryName)
+        {
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                string query = "SELECT * FROM Countries WHERE CountryID = @CountryID";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.Add("@CountryID", SqlDbType.Int).Value = CountryID;
+
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // The record was found
+
+                            CountryName = (string)reader["CountryName"];
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError($"Error: {ex}");
+                }
+            }
+            return false;
+        }
+
+        public static bool GetCountryInfoByName(string CountryName, ref int CountryID)
+        {
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                string query = "SELECT * FROM Countries WHERE CountryName = @CountryName";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.Add("@CountryName", SqlDbType.NVarChar, 50).Value = CountryName;
+
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // The record was found
+
+                            CountryID = (int)reader["CountryID"];
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError($"Error: {ex}");
+                }
+            }
+            return false;
+        }
+
+        public static DataTable GetAllCountries()
+        {
+                DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                string query = "SELECT * FROM Countries order by CountryName";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+
+                        {
+                            dt.Load(reader);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError($"Error: {ex}");
+                }
+            }
+            return dt;
+        }
+    }
+}
