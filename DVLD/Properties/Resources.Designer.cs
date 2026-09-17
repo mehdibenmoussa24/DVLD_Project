750f91b9f56f0c38b35dbe82e57662ebde2e3f7c
Mehdi Benmoussa <mben76337@gmail.com>
2026-09-17 10:37:01 +0100

    Add frmShowPersonInfo form and Person icon Add frmShowPersonInfo with UI, designer, and resources. Register the form and resources in .csproj. Add Person_ico image resource. Update resource files and image order. Fix Close button declaration order in frmFindPerson.

--- DVLD/Properties/Resources.Designer.cs
+++ DVLD/Properties/Resources.Designer.cs
@@ -263,6 +263,16 @@ namespace DVLD.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Person_ico {
+            get {
+                object obj = ResourceManager.GetObject("Person-ico", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Phone {
             get {
                 object obj = ResourceManager.GetObject("Phone", resourceCulture);
--- DVLD/frmShowPersonInfo.Designer.cs
+++ DVLD/frmShowPersonInfo.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DVLD
 {
-    partial class frmFindPerson
+    partial class frmShowPersonInfo
     {
         /// <summary>
         /// Required designer variable.
@@ -28,40 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFindPerson));
-            this.ctrlPersonCardWithFilter1 = new DVLD.ctrlPersonCardWithFilter();
             this.label1 = new System.Windows.Forms.Label();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Close = new System.Windows.Forms.Button();
+            this.ctrlPersonCard1 = new DVLD.ctrlPersonCard();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // ctrlPersonCardWithFilter1
-            // 
-            this.ctrlPersonCardWithFilter1.BackColor = System.Drawing.Color.White;
-            this.ctrlPersonCardWithFilter1.FilterEnabled = true;
-            this.ctrlPersonCardWithFilter1.Location = new System.Drawing.Point(12, 73);
-            this.ctrlPersonCardWithFilter1.Name = "ctrlPersonCardWithFilter1";
-            this.ctrlPersonCardWithFilter1.ShowAddPerson = true;
-            this.ctrlPersonCardWithFilter1.Size = new System.Drawing.Size(1395, 546);
-            this.ctrlPersonCardWithFilter1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(606, 9);
+            this.label1.Location = new System.Drawing.Point(638, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 54);
+            this.label1.Size = new System.Drawing.Size(145, 54);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Find";
+            this.label1.Text = "Person";
             // 
-            // imageList1
+            // pictureBox1
             // 
-            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::DVLD.Properties.Resources.Person_ico;
+            this.pictureBox1.Location = new System.Drawing.Point(582, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Close
             // 
@@ -69,27 +63,36 @@
             this.Close.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Close.Image = global::DVLD.Properties.Resources.CLose;
             this.Close.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.Close.Location = new System.Drawing.Point(1253, 625);
+            this.Close.Location = new System.Drawing.Point(1282, 512);
             this.Close.Name = "Close";
             this.Close.Size = new System.Drawing.Size(154, 51);
-            this.Close.TabIndex = 2;
+            this.Close.TabIndex = 3;
             this.Close.Text = "Close";
             this.Close.UseVisualStyleBackColor = false;
             this.Close.Click += new System.EventHandler(this.Close_Click);
             // 
-            // frmFindPerson
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.BackColor = System.Drawing.Color.White;
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(12, 89);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(1424, 412);
+            this.ctrlPersonCard1.TabIndex = 0;
+            // 
+            // frmShowPersonInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1427, 683);
+            this.ClientSize = new System.Drawing.Size(1453, 579);
             this.Controls.Add(this.Close);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.ctrlPersonCardWithFilter1);
+            this.Controls.Add(this.ctrlPersonCard1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "frmFindPerson";
+            this.Name = "frmShowPersonInfo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Find Person";
+            this.Text = "Person Details";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,9 +100,9 @@
 
         #endregion
 
-        private ctrlPersonCardWithFilter ctrlPersonCardWithFilter1;
+        private ctrlPersonCard ctrlPersonCard1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button Close;
     }
 }
