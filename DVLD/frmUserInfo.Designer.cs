24be9a8cd58fb8cea6ca33b4b0d45c89a62c26e7
Mehdi Benmoussa <mben76337@gmail.com>
2026-09-22 15:19:25 +0100

    Add frmUserInfo form for user details

---
+++ DVLD/frmUserInfo.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace DVLD
+{
+    partial class frmUserInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlUserCard1 = new DVLD.ctrlUserCard();
+            this.Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ctrlUserCard1
+            // 
+            this.ctrlUserCard1.BackColor = System.Drawing.Color.White;
+            this.ctrlUserCard1.Location = new System.Drawing.Point(3, 2);
+            this.ctrlUserCard1.Name = "ctrlUserCard1";
+            this.ctrlUserCard1.Size = new System.Drawing.Size(1423, 613);
+            this.ctrlUserCard1.TabIndex = 0;
+            // 
+            // Close
+            // 
+            this.Close.BackColor = System.Drawing.Color.Gainsboro;
+            this.Close.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Close.Image = global::DVLD.Properties.Resources.stop;
+            this.Close.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Close.Location = new System.Drawing.Point(1233, 609);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(179, 51);
+            this.Close.TabIndex = 4;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = false;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // frmUserInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1424, 672);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.ctrlUserCard1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmUserInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "frmUserInfo";
+            this.Load += new System.EventHandler(this.frmUserInfo_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlUserCard ctrlUserCard1;
+        private System.Windows.Forms.Button Close;
+    }
+}
