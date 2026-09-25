a64e046fa33fc8b9542c4b07e823dae085d40b96
Mehdi Benmoussa <mben76337@gmail.com>
2026-09-25 11:50:02 +0100

    Add frmListApplicationTypes form
    Add frmListApplicationTypes with code-behind, designer, and resources.
    Display application types in a DataGridView.
    Add record count and context menu actions.

---
+++ DVLD/frmListApplicationTypes.cs
@@ -0,0 +1,51 @@
+﻿using DVLD.Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD
+{
+    public partial class frmListApplicationTypes : Form
+    {
+        private DataTable _dtAllApplicationTypes;
+
+        public frmListApplicationTypes()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmListApplicationTypes_Load(object sender, EventArgs e)
+        {
+            _dtAllApplicationTypes = ApplicationType.GetAllApplicationTypes();
+            dgvApplicationTypes.DataSource = _dtAllApplicationTypes;
+            lblRecordsCount.Text = dgvApplicationTypes.Rows.Count.ToString();
+
+            dgvApplicationTypes.Columns[0].HeaderText = "ID";
+            dgvApplicationTypes.Columns[0].Width = 110;
+
+            dgvApplicationTypes.Columns[1].HeaderText = "Title";
+            dgvApplicationTypes.Columns[1].Width = 400;
+
+            dgvApplicationTypes.Columns[2].HeaderText = "Fees";
+            dgvApplicationTypes.Columns[2].Width = 100;
+
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Comming Soon", "Soon", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            frmListApplicationTypes_Load(null, null);
+        }
+    }
+}
