49d72d4006e8115b33a713f979767f3f1f12c4f2
Mehdi Benmoussa <mben76337@gmail.com>
2026-09-25 11:37:47 +0100

    Add ApplicationType business and data access layers
    Add ApplicationType business class with properties and CRUD operations.
    Add ApplicationTypeData for ADO.NET database operations.
    Integrate the business and data access layers.
    Add error logging for data access operations.

---
+++ DVLD.Business/ApplicationType.cs
@@ -0,0 +1,101 @@
+﻿using DVLD.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Business
+{
+    public class ApplicationType
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+
+        public int ID { set; get; }
+        public string Title { set; get; }
+        public float Fees { set; get; }
+
+        public ApplicationType()
+
+        {
+            this.ID = -1;
+            this.Title = "";
+            this.Fees = 0;
+            Mode = enMode.AddNew;
+
+        }
+
+        public ApplicationType(int ID, string ApplicationTypeTitel, float ApplicationTypeFees)
+
+        {
+            this.ID = ID;
+            this.Title = ApplicationTypeTitel;
+            this.Fees = ApplicationTypeFees;
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewApplicationType()
+        {
+            //call DataAccess Layer 
+
+            this.ID = ApplicationTypeData.AddNewApplicationType(this.Title, this.Fees);
+
+
+            return (this.ID != -1);
+        }
+
+        private bool _UpdateApplicationType()
+        {
+            //call DataAccess Layer 
+
+            return ApplicationTypeData.UpdateApplicationType(this.ID, this.Title, this.Fees);
+        }
+
+        public static ApplicationType Find(int ID)
+        {
+            string Title = ""; float Fees = 0;
+
+            if (ApplicationTypeData.GetApplicationTypeInfoByID((int)ID, ref Title, ref Fees))
+
+                return new ApplicationType(ID, Title, Fees);
+            else
+                return null;
+
+        }
+
+        public static DataTable GetAllApplicationTypes()
+        {
+            return ApplicationTypeData.GetAllApplicationTypes();
+
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewApplicationType())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateApplicationType();
+
+            }
+
+            return false;
+        }
+    }
+}
---
+++ DVLD.DataAccess/ApplicationTypeData.cs
@@ -0,0 +1,160 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.DataAccess
+{
+    public class ApplicationTypeData
+    {
+        public static bool GetApplicationTypeInfoByID(int ApplicationTypeID,
+           ref string ApplicationTypeTitle, ref float ApplicationFees)
+        {
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                string query = "SELECT * FROM ApplicationTypes WHERE ApplicationTypeID = @ApplicationTypeID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.Add("@ApplicationTypeID",SqlDbType.Int).Value = ApplicationTypeID;
+
+                    try
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                ApplicationTypeTitle = (string)reader["ApplicationTypeTitle"];
+                                ApplicationFees = Convert.ToSingle(reader["ApplicationFees"]);
+
+                                // The record was found
+                                return true;
+                            }
+                            else
+                            {
+                                // The record was not found
+                               return false;
+                            }
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                       Logger.LogError("Error: " + ex.Message);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static DataTable GetAllApplicationTypes()
+        {
+
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                string query = "SELECT * FROM ApplicationTypes order by ApplicationTypeTitle";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+
+                            {
+                                dt.Load(reader);
+                            }
+                        }
+                    }
+
+                    catch (Exception ex)
+                    {
+                      Logger.LogError("Error: " + ex.Message);
+                    }
+                }
+            }
+            return dt;
+
+        }
+
+        public static int AddNewApplicationType(string Title, float Fees)
+        {
+            int ApplicationTypeID = -1;
+
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                string query = @"Insert Into ApplicationTypes (ApplicationTypeTitle,ApplicationFees)
+                            Values (@Title,@Fees)
+                            
+                            SELECT SCOPE_IDENTITY();";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.Add("@ApplicationTypeTitle", SqlDbType.NVarChar, 150).Value = Title;
+                    command.Parameters.Add("@ApplicationFees", SqlDbType.SmallMoney).Value = Fees;
+                    try
+                    {
+                        connection.Open();
+
+                        object result = command.ExecuteScalar();
+
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            ApplicationTypeID = insertedID;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                       Logger.LogError("Error: " + ex.Message);
+
+                    }
+                }
+            }
+
+            return ApplicationTypeID;
+
+        }
+
+        public static bool UpdateApplicationType(int ApplicationTypeID, string Title, float Fees)
+        {
+
+            int rowsAffected = 0;
+            using (SqlConnection connection = new SqlConnection(DataAccessSettings.ConnectionString))
+            {
+                string query = @"Update  ApplicationTypes  
+                            set ApplicationTypeTitle = @Title,
+                                ApplicationFees = @Fees
+                                where ApplicationTypeID = @ApplicationTypeID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.Add("@ApplicationTypeID", SqlDbType.Int).Value = ApplicationTypeID;
+                    command.Parameters.Add("@ApplicationTypeTitle", SqlDbType.NVarChar, 150).Value = Title;
+                    command.Parameters.Add("@ApplicationFees", SqlDbType.SmallMoney).Value = Fees;
+
+                    try
+                    {
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogError("Error: " + ex.Message);
+                        return false;
+                    }
+                }
+            }
+            return (rowsAffected > 0);
+        }
+    }
+}
